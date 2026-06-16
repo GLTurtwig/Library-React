0bd9893ecd797b0444ce3b0516e912ca5e87bc95
GLTurtwig <xjondarx@gmail.com>
2026-06-16 03:25:58 -0500

    Init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import { books } from './data'
+
+import Nav from "./components/Nav";
+import Footer from "./components/Footer.jsx"
+
+import Home from "./pages/Home.jsx"
+import Books from "./pages/Books.jsx"
+import BookInfo from "./pages/BookInfo.jsx"
+import Cart from "./pages/Cart.jsx"
+
+
+
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book){
+    setCart([...cart, {...book, quantity: +1}]);
+  }
+
+function changeQuantity(book, quantity){
+  setCart(cart.map((item) => 
+    item.id === book.id ? {
+      ...item, quantity: +quantity
+    } : 
+      item
+  ))
+}
+
+function removeItem(book){
+  setCart(cart.filter(item => item.id !== book.id))
+}
+
+  useEffect(() => {
+
+  }, [cart])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav cart={cart}/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books}/>}/>
+          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart} cart={cart} />}/>
+          <Route path="/cart" element={<Cart books={books} cart={cart} changeQuantity={changeQuantity} removeItem={removeItem}/>}/>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Nav.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom'
+import LibraryLogo from '../assets/Library.svg'
+
+function Nav({cart}){
+    function openMenu(){
+        document.body.classList += " menu--open";
+    } 
+
+    function closeMenu (){
+        document.body.classList.remove("menu--open");
+    }
+
+    const cartNumber = () => {
+        let totalItems = 0;
+        cart.map(book => {
+            totalItems += book.quantity;
+        })
+        return totalItems;
+    }
+
+    return (
+        <nav>
+            <div className="nav__container"> 
+                <Link to="/">
+                    <img src={LibraryLogo} alt="" className="logo"></img>
+                </Link>
+                <ul className="nav__links">
+                    <li className="nav__list">
+                        <Link to="/" className="nav__link"> Home </Link>
+                        <Link to="/books" className="nav__link"> Books </Link>
+                    </li>
+                    <button className="btn__menu" onClick={openMenu}> 
+                        <FontAwesomeIcon icon="bars" />
+                    </button>
+                    <li className="nav__icon">
+                        <Link to="/cart" className="nav__link">
+                            <FontAwesomeIcon icon="shopping-cart" />
+                        </Link>
+                        {
+                            cartNumber() > 0 && <span className="cart__length">{cartNumber()}</span>
+                        }
+                    </li>
+                </ul>
+                <div className="menu__backdrop">
+                    <button className="btn__menu btn__menu--close">
+                        <FontAwesomeIcon icon="times" onClick={closeMenu}/>
+                    </button>
+                    <ul className="menu__links">
+                        <li className="menu__list">
+                            <Link to="/" className="menu__link">Home</Link>
+                        </li>
+                        <li className="menu__list">
+                            <Link to="/books" className="menu__link">Books</Link>
+                        </li>
+                        <li className="menu__list">
+                            <Link to="/" className="menu__link">Cart</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
